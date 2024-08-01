6076c03100ab52eab2f7d47f3f778162bfbaa85f
Mario Roy <marioeroy@gmail.com>
2024-08-01 13:58:37 -0500

    Update

--- Examples/taichi_cpu.py
+++ Examples/taichi_cpu.py
@@ -13,19 +13,25 @@
 import argparse
 import taichi as ti
 import numpy as np
+import os
 import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--workers", type=int, default=os.cpu_count())
+parser.add_argument("--arraysize", type=int, default=100_000_000)
+args = parser.parse_args()
+
+# If the kernel is run on the CPU backend, N threads will be used
+num_threads = int(max(1, args.workers))
 
 @ti.kernel
 def compute_inplace(array: ti.types.ndarray()):
     m = array.shape[0]
+    ti.loop_config(parallelize=num_threads)
     for i in range(m):
         array[i] = ti.cos(ti.sin(ti.sqrt(array[i])))
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--arraysize", type=int, default=100_000_000)
-    args = parser.parse_args()
-
     # Generate the data structures for the benchmark
     array0 = np.random.rand(args.arraysize).astype(np.float32)
     array_copy = np.array(array0)
